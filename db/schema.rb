434fa7c6a1eda742115431e5e94a1861f2102abc
notonlyvandalzzz <abs.deranger@gmail.com>
2016-08-25 16:40:08 +1000

    1.1

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160825033622) do
+ActiveRecord::Schema.define(version: 20160825040151) do
 
   create_table "products", force: :cascade do |t|
     t.text     "title"
