22fa2af724a761a7b58481a832ab4cb5a24f3def
NotOnlyVandalzzz <abs.deranger@gmail.com>
2016-08-25 13:51:49 +1000

    Migr mod

--- db/migrate/20160825033622_create_products.rb
+++ db/migrate/20160825033622_create_products.rb
@@ -1,4 +1,15 @@
 class CreateProducts < ActiveRecord::Migration
   def change
+  	create_table :products do |t|
+  		t.text :title
+  		t.integer :price
+  		t.text :size
+  		t.text :desc
+  		t.boolean :is_spicy
+  		t.boolean :is_veget
+  		t.boolean :is_halal
+  		t.string :path_to_pic
+  		t.timestamps
+  	end
   end
 end
