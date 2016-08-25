c582b7459482c913252f9f0c6abd545a055a33ff
NotOnlyVandalzzz <abs.deranger@gmail.com>
2016-08-25 11:49:56 +1000

    1.4

--- public/app.js
+++ public/app.js
@@ -1,7 +1,7 @@
 
 function something() {
 	var x = window.localStorage.getItem('aaa');
-
-	window.localStorage.setItem('aaa', 555);
-		alert (x);
+	x = x + 1;
+	window.localStorage.setItem('aaa', x);
+	alert (x);
 	}
