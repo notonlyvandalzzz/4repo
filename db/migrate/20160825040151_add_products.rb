020257e7bce2da66e23801fb68651c8ee6cffb34
NotOnlyVandalzzz <abs.deranger@gmail.com>
2016-08-26 12:44:36 +1000

    Revert "1.2"
    This reverts commit 73e9f487f97b4907d6cbe4f1eacefa2ac778ca96.

--- db/migrate/20160825040151_add_products.rb
+++ db/migrate/20160825040151_add_products.rb
@@ -1,15 +1,6 @@
 class AddProducts < ActiveRecord::Migration
   def change
-  	Product.create ({
-  		:title => 'Гавайская', 
-  		:desc => 'Нежная пицца с курицей и кусочками ананасов', 
-  		:size => 'large', 
-  		:price => '430', 
-  		:is_spicy => false, 
-  		:is_veget => false, 
-  		:is_halal => true, 
-  		:path_to_pic => '/images/hawaiian.jpg'
-  		})
+  	Product.create :title => 'Гавайская', :desc => 'Нежная пицца с курицей и кусочками ананасов', :size => 'large', :price => '430', :is_spicy => false, :is_veget => false, :is_halal => true, :path_to_pic => '/images/hawaiian.jpg'
 
   	Product.create :title => 'Хрустящая', :desc => 'Хрустящая пицца с грибами и рукколой', :size => 'medium', :price => '390', :is_spicy => false,	:is_veget => true, :is_halal => false, :path_to_pic => '/images/crusty.png'
 
