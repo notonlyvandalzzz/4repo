740cdf3d54bcb337c91429ba1bf582837e84f31a
NotOnlyVandalzzz <abs.deranger@gmail.com>
2016-08-26 16:44:50 +1000

    1.11

--- public/app.js
+++ public/app.js
@@ -7,15 +7,21 @@ function something() {
 	}
 
 function addtocart(id) {
+	
 	var key = 'pzshp_product_' + id;
 	var x = window.localStorage.getItem(key);
 	x = x * 1 + 1;
 	window.localStorage.setItem(key, x);
+	
+	// alert ('You\'ve added one pizza with id ' + id);
+}
+
+function countcart() {
+	var tot = 0;
 	for (var i = 0; i < localStorage.length; i++){
 		if (key.search('pzshp_product') >= 0) {
 			tot = tot * 1 + window.localStorage[key] * 1;
 		}
 	}
-	alert ('Total items: ' + tot);
-	// alert ('You\'ve added one pizza with id ' + id);
+	document.getElementById('basket').innerHTML = 'Total ' + tot;
 }
