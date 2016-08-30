32fea843c9fbd148f6b52743fb807faf2d23d541
NotOnlyVandalzzz <abs.deranger@gmail.com>
2016-08-30 14:50:18 +1000

    migration fill

--- db/migrate/20160830044414_create_orders.rb
+++ db/migrate/20160830044414_create_orders.rb
@@ -1,4 +1,13 @@
 class CreateOrders < ActiveRecord::Migration
   def change
+  	create_table :orders do |t|
+  		t.text :name
+  		t.text :address
+  		t.text :phone
+  		t.text :orders_list
+  		t.text :grandtotal
+
+  		t.timestamps
+  	end
   end
 end
